f17ecb0138d0fa724b0d0f0f7e73e66ca9112af6
Jens Bertram <code@jens-bertram.net>
2014-01-14 14:39:53 +0100

    update run script to be more general

--- runMain.sh
+++ runMain.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Query: $1
-mvn exec:java -Dexec.mainClass="de.unihildesheim.lucene.queryclarity.Main" -Dexec.args="-index /media/Brain2/CLEF/solr/solr-4.6.0/example/clef/db/data/index -query '$1'"
+mvn exec:java -Dexec.mainClass="de.unihildesheim.lucene.queryclarity.Main" -Dexec.args="-index ./data/index -query '$1'"
