abde5f770658c2460479aa28de490bf13d4364e4
Jens Bertram <code@jens-bertram.net>
2015-01-20 12:31:29 +0100

    documentation

--- src/main/java/de/unihildesheim/iw/fiz/Defaults.java
+++ src/main/java/de/unihildesheim/iw/fiz/Defaults.java
@@ -18,6 +18,9 @@
 package de.unihildesheim.iw.fiz;
 
 /**
+ * Default values for interacting with the ES based document repository and the
+ * scoring tools.
+ *
  * @author Jens Bertram (code@jens-bertram.net)
  */
 public class Defaults {
@@ -25,8 +28,25 @@ public class Defaults {
    * Languages provided by ES index.
    */
   public enum SRC_LANGUAGE {
-    DE, EN, FR;
+    /**
+     * German.
+     */
+    DE,
+    /**
+     * English
+     */
+    EN,
+    /**
+     * French
+     */
+    FR;
 
+    /**
+     * Try to get a language by it's name.
+     *
+     * @param lng Language identifier as string
+     * @return Language or {@code null} if none was found for the given string
+     */
     public static SRC_LANGUAGE getByString(final String lng) {
       for (final SRC_LANGUAGE srcLng : SRC_LANGUAGE.values()) {
         if (lng.equalsIgnoreCase(srcLng.name())) {
@@ -37,34 +57,58 @@ public class Defaults {
     }
   }
 
-  /** ES settings. (TODO: make these external) */
+  /**
+   * ES settings. (TODO: make these external)
+   */
   @SuppressWarnings("PublicInnerClass")
   public static final class ES_CONF {
     // basic settings
-    /** Name of the index to query. */
+    /**
+     * Name of the index to query.
+     */
     public static final String INDEX = "epfull_repo";
-    /** URL to reach the index. */
+    /**
+     * URL to reach the index.
+     */
     public static final String URL = "http://t4p.fiz-karlsruhe.de:80";
-    /** Type of document to query for. */
+    /**
+     * Type of document to query for.
+     */
     public static final String DOC_TYPE = "patent";
-    /** Number of results to get from each shard. */
+    /**
+     * Number of results to get from each shard.
+     */
     public static final int PAGE_SIZE = 500;
-    /** How long to keep the scroll open. */
+    /**
+     * How long to keep the scroll open.
+     */
     public static final String SCROLL_KEEP = "15m";
 
     // document fields
-    /** Field name containing the document id. */
+    /**
+     * Field name containing the document id.
+     */
     public static final String FLD_DOCID = "_id";
-    /** Prefix name of the claims field. */
+    /**
+     * Prefix name of the claims field.
+     */
     public static final String FLD_CLAIM_PREFIX = "CLM";
-    /** Field name holding the description. */
+    /**
+     * Field name holding the description.
+     */
     public static final String FLD_DESC = "DETD";
-    /** Field name holding the description language used. */
+    /**
+     * Field name holding the description language used.
+     */
     public static final String FLD_DESC_LNG = "DETDL";
-    /** Field holding a reference to the patent. */
+    /**
+     * Field holding a reference to the patent.
+     */
     public static final String FLD_PATREF = "PN";
 
-    /** How many times to retry a connection. */
+    /**
+     * How many times to retry a connection.
+     */
     public static final int MAX_RETRY = 15;
   }
 }
