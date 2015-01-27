deffe4d4b2d7a7696ef4d7083b7889dd7f9a9133
Jens Bertram <code@jens-bertram.net>
2015-01-27 09:48:26 +0100

    add generic filtered terms iterator

--- src/main/java/de/unihildesheim/iw/lucene/scoring/data/AbstractVocabularyProvider.java
+++ src/main/java/de/unihildesheim/iw/lucene/scoring/data/AbstractVocabularyProvider.java
@@ -17,8 +17,14 @@
 
 package de.unihildesheim.iw.lucene.scoring.data;
 
+import de.unihildesheim.iw.ByteArray;
+import de.unihildesheim.iw.lucene.index.DataProviderException;
 import de.unihildesheim.iw.lucene.index.IndexDataProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Set;
 
@@ -67,5 +73,78 @@ public abstract class AbstractVocabularyProvider<T extends VocabularyProvider>
   public T documentIds(final Set<Integer> documentIds) {
     this.docIds = Objects.requireNonNull(documentIds);
     return getThis();
+  }
+
+  /**
+   * Iterator wrapping another terms iterator and provides basic term filtering.
+   */
+  public static class FilteredTermsIterator
+      implements Iterator<ByteArray> {
+    /**
+     * Logger instance for this class.
+     */
+    private static final Logger LOG =
+        LoggerFactory.getLogger(FilteredTermsIterator.class);
+    /**
+     * Filter for excluding terms.
+     */
+    private final Filter filter;
+    /**
+     * Original terms iterator.
+     */
+    private final Iterator<ByteArray> termsIt;
+    /**
+     * Next term in iteration.
+     */
+    private ByteArray nextTerm;
+    /**
+     * Current iteration term.
+     */
+    private ByteArray term;
+
+    /**
+     * Initialize the iterator.
+     * @param iterator Original iterator
+     * @param termFilter Filter to exclude terms
+     * @throws DataProviderException Forwarded from filter
+     */
+    public FilteredTermsIterator(
+        final Iterator<ByteArray> iterator, final Filter termFilter)
+        throws DataProviderException {
+      this.filter = Objects.requireNonNull(termFilter);
+      this.termsIt = Objects.requireNonNull(iterator);
+      setNext();
+    }
+
+    /**
+     * Get the next iteration element.
+     * @throws DataProviderException Forwarded from filter
+     */
+    private void setNext()
+        throws DataProviderException {
+      this.nextTerm = null;
+      while (this.termsIt.hasNext() && this.nextTerm == null) {
+        this.nextTerm = this.filter.filter(this.termsIt.next());
+      }
+    }
+
+    @Override
+    public boolean hasNext() {
+      return this.nextTerm != null;
+    }
+
+    @Override
+    public ByteArray next() {
+      this.term = this.nextTerm;
+      if (this.term == null) {
+        throw new NoSuchElementException();
+      }
+      try {
+        setNext();
+      } catch (DataProviderException e) {
+        LOG.error("Failed to get next term.", e);
+      }
+      return term;
+    }
   }
 }
