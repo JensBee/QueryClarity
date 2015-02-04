b467914a74d927febddb3cde471e3cb0c85648ca
Jens Bertram <code@jens-bertram.net>
2015-02-04 16:18:52 +0100

    add low precision mode

--- src/main/java/de/unihildesheim/iw/lucene/scoring/clarity/ImprovedClarityScore.java
+++ src/main/java/de/unihildesheim/iw/lucene/scoring/clarity/ImprovedClarityScore.java
@@ -31,6 +31,7 @@ import de.unihildesheim.iw.lucene.scoring.data.DefaultVocabularyProvider;
 import de.unihildesheim.iw.lucene.scoring.data.FeedbackProvider;
 import de.unihildesheim.iw.lucene.scoring.data.VocabularyProvider;
 import de.unihildesheim.iw.util.MathUtils.KlDivergence;
+import de.unihildesheim.iw.util.MathUtils.KlDivergenceLowPrecision;
 import de.unihildesheim.iw.util.StringUtils;
 import de.unihildesheim.iw.util.TimeMeasure;
 import org.apache.lucene.analysis.Analyzer;
@@ -46,8 +47,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Improved Clarity Score implementation as described by Hauff, Murdock,
@@ -79,6 +80,11 @@ public final class ImprovedClarityScore
       GlobalConfiguration.conf().getString(
           DefaultKeys.MATH_CONTEXT.toString()));
   /**
+   * If true, low precision math is used for doing calculations.
+   */
+  private static final boolean MATH_LOW_PRECISION = GlobalConfiguration.conf()
+      .getBoolean(DefaultKeys.MATH_LOW_PRECISION.toString(), false);
+  /**
    * {@link IndexDataProvider} to use.
    */
   private final IndexDataProvider dataProv;
@@ -100,18 +106,60 @@ public final class ImprovedClarityScore
   private final ImprovedClarityScoreConfiguration conf;
 
   /**
-   * Class wrapping all methods needed for calculation needed models. Also holds
-   * results of the calculations.
+   * Abstract model class. Shared methods for low/high precision model
+   * calculations.
    */
-  private class Model {
+  private abstract class AbstractModel {
     /**
      * List of query terms issued.
      */
-    private final Collection<ByteArray> queryTerms;
+    final Collection<ByteArray> queryTerms;
     /**
      * List of feedback documents to use.
      */
-    private final Collection<Integer> feedbackDocs;
+    final Collection<Integer> feedbackDocs;
+    /**
+     * Stores the feedback document models.
+     */
+    final Map<Integer, DocumentModel> docModels;
+
+    /**
+     * Initialize the abstract model with a set of query terms and feedback
+     * documents.
+     *
+     * @param qt Query terms
+     * @param fb Feedback documents
+     */
+    AbstractModel(
+        final Collection<ByteArray> qt, final Collection<Integer> fb) {
+      LOG.debug("Create runtime cache.");
+      // add query terms, skip those not in index
+      this.queryTerms = new ArrayList<>(qt.size());
+      this.queryTerms.addAll(qt.stream().filter(queryTerm ->
+          ImprovedClarityScore.this.dataProv.metrics().tf(queryTerm) > 0L)
+          .collect(Collectors.toList()));
+
+      // add feedback documents
+      this.feedbackDocs = new ArrayList<>(fb.size());
+      this.feedbackDocs.addAll(fb);
+
+      this.docModels = new ConcurrentHashMap<>((int) (
+          (double) this.feedbackDocs.size() * 1.8));
+
+      LOG.info("Caching document models");
+      for (final Integer docId : this.feedbackDocs) {
+        this.docModels.put(docId,
+            ImprovedClarityScore.this.dataProv.metrics().docData(docId));
+      }
+    }
+  }
+
+  /**
+   * Class wrapping all methods needed for calculation needed models. Also holds
+   * results of the calculations.
+   */
+  private class Model
+      extends AbstractModel {
     /**
      * Stores the static part of the query model calculation for each document.
      */
@@ -126,19 +174,6 @@ public final class ImprovedClarityScore
      * Lambda, [3] 1 - Beta, [4] 1 - Lambda
      */
     private final BigDecimal[] dmParams;
-    /**
-     * Caches values of document and query models. Those need to be normalized
-     * before calculating the score.
-     */
-    private final Map<Long, Tuple2<BigDecimal, BigDecimal>> dataSets;
-    /**
-     * Counter for entries in {@link #dataSets}. Gets used as map key.
-     */
-    private final AtomicLong dataSetCounter;
-    /**
-     * Stores the feedback document models.
-     */
-    private final Map<Integer, DocumentModel> docModels;
 
     /**
      * Initialize the model calculation object.
@@ -153,7 +188,7 @@ public final class ImprovedClarityScore
     private Model(final Collection<ByteArray> qt,
         final Collection<Integer> fb,
         final double dmSmoothing, final double dmBeta, final double dmLambda) {
-      LOG.debug("Create runtime cache.");
+      super(qt, fb);
 
       this.dmParams = new BigDecimal[]{
           BigDecimal.valueOf(dmSmoothing),
@@ -166,30 +201,17 @@ public final class ImprovedClarityScore
       this.dmParams[4] = BigDecimal.ONE.subtract(
           this.dmParams[2], MATH_CONTEXT);
 
-      // add query terms, skip those not in index
-      this.queryTerms = new ArrayList<>(qt.size());
-      this.queryTerms.addAll(qt.stream().filter(queryTerm ->
-          ImprovedClarityScore.this.dataProv.metrics().tf(queryTerm) > 0L)
-          .collect(Collectors.toList()));
-
-      // add feedback documents
-      this.feedbackDocs = new ArrayList<>(fb.size());
-      this.feedbackDocs.addAll(fb);
-
       // initialize other properties
       this.staticQueryModelParts = new ConcurrentHashMap<>((int) (
           (double) this.feedbackDocs.size() * 1.8));
       this.staticSmoothingParts = new ConcurrentHashMap<>((int) (
           (double) this.feedbackDocs.size() * 1.8));
-      this.docModels = new ConcurrentHashMap<>((int) (
-          (double) this.feedbackDocs.size() * 1.8));
-      this.dataSets = new ConcurrentHashMap<>(2000);
-      this.dataSetCounter = new AtomicLong(0L);
 
       LOG.info("Pre-calculating static query model and smoothing values");
       for (final Integer docId : this.feedbackDocs) {
         final DocumentModel docModel = ImprovedClarityScore.this.dataProv
             .metrics().docData(docId);
+
         // smoothing value
         final BigDecimal sSmooth = BigDecimal.valueOf(docModel.tf())
             .add(this.dmParams[0]
@@ -204,12 +226,6 @@ public final class ImprovedClarityScore
               document(docModel, queryTerm), MATH_CONTEXT);
         }
         this.staticQueryModelParts.put(docId, staticPart);
-      }
-
-      LOG.info("Caching document models");
-      for (final Integer docId : this.feedbackDocs) {
-        this.docModels.put(docId,
-            ImprovedClarityScore.this.dataProv.metrics().docData(docId));
       }
     }
 
@@ -251,17 +267,108 @@ public final class ImprovedClarityScore
           .map(d -> document(this.docModels.get(d), term)
               .multiply(this.staticQueryModelParts.get(d),
                   MATH_CONTEXT))
-          .reduce((x, y) -> x.add(y, MATH_CONTEXT)).get();
-      /*
-      BigDecimal result = BigDecimal.ZERO;
+          .reduce(BigDecimal.ZERO, (sum, qm) -> sum.add(qm, MATH_CONTEXT));
+      //.reduce((sum, qm) -> sum.add(qm, MATH_CONTEXT)).get();
+    }
+  }
 
+  /**
+   * Class wrapping all methods needed for low-precision calculation of model
+   * values. Also holds results of the calculations.
+   */
+  private final class ModelLowPrecision
+      extends AbstractModel {
+    /**
+     * Stores the static part of the query model calculation for each document.
+     */
+    private final Map<Integer, Double> staticQueryModelParts;
+    /**
+     * Stores the static part of the smoothed model calculation for each
+     * document.
+     */
+    private final Map<Integer, Double> staticSmoothingParts;
+    /**
+     * Document model calculation parameters: [0] Smoothing (mu), [1] Beta, [2]
+     * Lambda, [3] 1 - Beta, [4] 1 - Lambda
+     */
+    private final double[] dmParams;
+
+    /**
+     * Initialize the model calculation object.
+     *
+     * @param qt Query terms. Query terms not found in the collection (TF=0)
+     * will be skipped.
+     * @param fb Feedback documents
+     * @param dmSmoothing Document model: Smoothing parameter value
+     * @param dmBeta Document model: Beta parameter value
+     * @param dmLambda Document model: Lambda parameter value
+     */
+    private ModelLowPrecision(final Collection<ByteArray> qt,
+        final Collection<Integer> fb,
+        final double dmSmoothing, final double dmBeta, final double dmLambda) {
+      super(qt, fb);
+
+      this.dmParams = new double[]{dmSmoothing, dmBeta, dmLambda,
+          (1d - dmBeta), (1d - dmLambda) };
+
+      // initialize other properties
+      this.staticQueryModelParts = new ConcurrentHashMap<>((int) (
+          (double) this.feedbackDocs.size() * 1.8));
+      this.staticSmoothingParts = new ConcurrentHashMap<>((int) (
+          (double) this.feedbackDocs.size() * 1.8));
+
+      LOG.info("Pre-calculating static query model and smoothing values");
       for (final Integer docId : this.feedbackDocs) {
-        result = result.add(document(this.docModels.get(docId), term)
-            .multiply(this.staticQueryModelParts.get(docId),
-                MATH_CONTEXT), MATH_CONTEXT);
+        final DocumentModel docModel = ImprovedClarityScore.this.dataProv
+            .metrics().docData(docId);
+
+        // smoothing value
+        final double sSmooth = docModel.tf() +
+            (this.dmParams[0] * docModel.termCount());
+        this.staticSmoothingParts.put(docId, sSmooth);
+
+        // static query model part (needs smoothing values)
+        double staticPart = 1d;
+        for (final ByteArray queryTerm : this.queryTerms) {
+          staticPart *= document(docModel, queryTerm);
+        }
+        this.staticQueryModelParts.put(docId, staticPart);
       }
-      return result;
-      */
+    }
+
+    /**
+     * Document model.
+     *
+     * @param docModel Document data model
+     * @param term Term to calculate the document model value for
+     * @return Document model value
+     */
+    final double document(
+        final DocumentModel docModel, final ByteArray term) {
+      // collection model of current term
+      final double cModel = ImprovedClarityScore.this
+          .dataProv.metrics().relTf(term);
+
+      // smoothed document-term model
+      final double smoothing = (docModel.tf(term) + (this.dmParams[0] *
+          cModel)) / this.staticSmoothingParts.get(docModel.id);
+
+      // final model calculation
+      return (this.dmParams[2] * ((this.dmParams[1] * smoothing) +
+          (this.dmParams[3] * cModel))) + (this.dmParams[4] * cModel);
+    }
+
+    /**
+     * Query model for all feedback documents.
+     *
+     * @param term Term to calculate the query model value for
+     * @return Query model value for all feedback documents
+     */
+    final double query(final ByteArray term) {
+      return this.feedbackDocs.stream()
+          .map(d -> document(this.docModels.get(d), term) *
+              this.staticQueryModelParts.get(d))
+          .reduce(0d, Double::sum);
     }
   }
 
@@ -390,28 +497,53 @@ public final class ImprovedClarityScore
     LOG.debug("Feedback term Document frequency threshold: {}%-{}%",
         minFreq.doubleValue() * 100d, maxFreq.doubleValue() * 100d);
 
-    final Model model = new Model(queryTerms, feedbackDocIds,
-        this.conf.getDocumentModelSmoothingParameter(), this.conf
-        .getDocumentModelParamBeta(), this.conf.getDocumentModelParamLambda());
-
-    LOG.info("Calculating query models using feedback vocabulary.");
-    List<Tuple2<BigDecimal, BigDecimal>> dataSets = this.vocProvider
+    // feedback terms stream used for calculation
+    final Stream<ByteArray> fbTermStream = this.vocProvider
         .documentIds(feedbackDocIds)
         .get()
         .filter(t -> {
           final BigDecimal relDf = this.dataProv.metrics().relDf(t);
           return (relDf.compareTo(minFreq) >= 0
               && relDf.compareTo(maxFreq) <= 0);
-        })
-        .map(term ->
-            Tuple.tuple2(
-                model.query(term), BigDecimal.valueOf(
-                    this.dataProv.metrics().relTf(term))))
-        .collect(Collectors.toList());
+        });
 
-    LOG.info("Calculating final score.");
-    result.setScore(
-        KlDivergence.sumAndCalc(dataSets).doubleValue());
+    if (MATH_LOW_PRECISION) {
+      final ModelLowPrecision model = new ModelLowPrecision(
+          queryTerms, feedbackDocIds,
+          this.conf.getDocumentModelSmoothingParameter(),
+          this.conf.getDocumentModelParamBeta(),
+          this.conf.getDocumentModelParamLambda());
+
+      LOG.info("Calculating query models using feedback vocabulary. " +
+          "(low precision)");
+      final List<Tuple2<Double, Double>> dataSets = fbTermStream
+          .map(term ->
+              Tuple.tuple2(
+                  model.query(term), this.dataProv.metrics().relTf(term)))
+          .collect(Collectors.toList());
+
+      LOG.info("Calculating final score.");
+      result.setScore(
+          KlDivergenceLowPrecision.sumAndCalc(dataSets));
+    } else {
+      final Model model = new Model(queryTerms, feedbackDocIds,
+          this.conf.getDocumentModelSmoothingParameter(),
+          this.conf.getDocumentModelParamBeta(),
+          this.conf.getDocumentModelParamLambda());
+
+      LOG.info("Calculating query models using feedback vocabulary. " +
+          "(high precision)");
+      final List<Tuple2<BigDecimal, BigDecimal>> dataSets = fbTermStream
+          .map(term ->
+              Tuple.tuple2(
+                  model.query(term), BigDecimal.valueOf(
+                      this.dataProv.metrics().relTf(term))))
+          .collect(Collectors.toList());
+
+      LOG.info("Calculating final score.");
+      result.setScore(
+          KlDivergence.sumAndCalc(dataSets).doubleValue());
+    }
 
     LOG.debug("Calculating improved clarity score for query {} "
             + "with {} document models took {}. {}",
