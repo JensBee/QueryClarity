8e7e5a684bc63e072653d3944782746d56479991
Jens Bertram <code@jens-bertram.net>
2015-03-10 10:29:12 +0100

    update DocumentModel test

--- src/test/java/de/unihildesheim/iw/lucene/document/DocumentModelTest.java
+++ src/test/java/de/unihildesheim/iw/lucene/document/DocumentModelTest.java
@@ -17,26 +17,298 @@
 
 package de.unihildesheim.iw.lucene.document;
 
+import de.unihildesheim.iw.lucene.document.DocumentModel.Builder;
+import org.apache.lucene.util.BytesRef;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("JavaDoc")
 public class DocumentModelTest {
 
   @Test
-  public void testSetTermFrequency()
+  public void builder_testSetTermFrequency()
       throws Exception {
+    final int dom_id = 1;
+    final Builder builder = new Builder(dom_id);
+    builder.setTermFrequency(new BytesRef("foo"), 12L);
+    builder.setTermFrequency(new BytesRef("bar"), 10L);
+    builder.setTermFrequency(new BytesRef("baz"), 3L);
 
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        3L, dom.tf(new BytesRef("baz")));
   }
 
   @Test
-  public void testSetTermFrequency1()
+  public void builder_testSetTermFrequency_nonUnique()
       throws Exception {
+    final int dom_id = 1;
+    final Builder builder = new Builder(dom_id);
+    builder.setTermFrequency(new BytesRef("foo"), 12L);
+    builder.setTermFrequency(new BytesRef("bar"), 10L);
+    builder.setTermFrequency(new BytesRef("baz"), 3L);
 
+    try {
+      builder.setTermFrequency(new BytesRef("foo"), 22L);
+      Assert.fail("Non unique term was successful added. " +
+          "Expected an exception to be thrown.");
+    } catch (final IllegalArgumentException e) {
+      // pass
+    }
   }
 
   @Test
-  public void testGetModel()
+  public void builder_testSetTermFrequency_lt0()
       throws Exception {
+    final int dom_id = 1;
+    final Builder builder = new Builder(dom_id);
+    builder.setTermFrequency(new BytesRef("foo"), 12L);
+    builder.setTermFrequency(new BytesRef("bar"), 10L);
 
+    try {
+      builder.setTermFrequency(new BytesRef("baz"), -1L);
+      Assert.fail("Term with negative term frequency value was successful " +
+          "added. Expected an exception to be thrown.");
+    } catch (final IllegalArgumentException e) {
+      // pass
+    }
+  }
+
+  @SuppressWarnings("ImplicitNumericConversion")
+  @Test
+  public void builder_testSetTermFrequency_eq0()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+    // number of unique terms
+    final int dom_termCount = 2; // one is zero
+    // total term frequency
+    final long dom_tf = 22L;
+
+    final Builder builder = new Builder(dom_id);
+    builder.setTermFrequency(new BytesRef("foo"), 12L);
+    builder.setTermFrequency(new BytesRef("bar"), 10L);
+    builder.setTermFrequency(new BytesRef("baz"), 0L);
+
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Unique term count differs.",
+        dom_termCount, dom.termCount());
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        0L, dom.tf(new BytesRef("baz")));
+
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 12L / (double) dom_tf, dom.relTf(new BytesRef("foo")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 10L / (double) dom_tf, dom.relTf(new BytesRef("bar")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 0L / (double) dom_tf, dom.relTf(new BytesRef("baz")), 0d);
+
+    Assert.assertEquals("Total term frequency differs.", dom_tf, dom.tf());
+  }
+
+  @Test
+  public void builder_testSetTermFrequencyMap()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+    // number of unique terms
+    final int dom_termCount = 2; // one is zero
+    // total term frequency
+    final long dom_tf = 25L;
+
+    final Builder builder = new Builder(dom_id);
+
+    final Map<BytesRef, Long> tfMap = new HashMap<>(6);
+    tfMap.put(new BytesRef("foo"), 12L);
+    tfMap.put(new BytesRef("bar"), 10L);
+    tfMap.put(new BytesRef("baz"), 3L);
+
+    builder.setTermFrequency(tfMap);
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        3L, dom.tf(new BytesRef("baz")));
+
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 12L / (double) dom_tf, dom.relTf(new BytesRef("foo")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 10L / (double) dom_tf, dom.relTf(new BytesRef("bar")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 3L / (double) dom_tf, dom.relTf(new BytesRef("baz")), 0d);
+
+    Assert.assertEquals("Total term frequency differs.", dom_tf, dom.tf());
+  }
+
+  @Test
+  public void builder_testSetTermFrequencyMap_eq0()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+    // number of unique terms
+    final int dom_termCount = 2; // one is zero
+    // total term frequency
+    final long dom_tf = 22L;
+    final Builder builder = new Builder(dom_id);
+
+    final Map<BytesRef, Long> tfMap = new HashMap<>(6);
+    tfMap.put(new BytesRef("foo"), 12L);
+    tfMap.put(new BytesRef("bar"), 10L);
+    tfMap.put(new BytesRef("baz"), 0L);
+
+    builder.setTermFrequency(tfMap);
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        0L, dom.tf(new BytesRef("baz")));
+
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 12L / (double) dom_tf, dom.relTf(new BytesRef("foo")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 10L / (double) dom_tf, dom.relTf(new BytesRef("bar")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 0L / (double) dom_tf, dom.relTf(new BytesRef("baz")), 0d);
+
+    Assert.assertEquals("Total term frequency differs.", dom_tf, dom.tf());
+  }
+
+  @Test
+  public void builder_testSetTermFrequencyMap_lt0()
+      throws Exception {
+    final int dom_id = 1;
+    final Builder builder = new Builder(dom_id);
+
+    final Map<BytesRef, Long> tfMap = new HashMap<>(6);
+    tfMap.put(new BytesRef("foo"), 12L);
+    tfMap.put(new BytesRef("bar"), 10L);
+    tfMap.put(new BytesRef("baz"), -3L);
+
+    try {
+      builder.setTermFrequency(tfMap);
+      Assert.fail("Term with negative term frequency value was successful " +
+          "added. Expected an exception to be thrown.");
+    } catch (final IllegalArgumentException e) {
+      // pass
+    }
+  }
+
+  @SuppressWarnings("ImplicitNumericConversion")
+  @Test
+  public void builder_testGetModel()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+    // number of unique terms
+    final int dom_termCount = 3;
+    // total term frequency
+    final long dom_tf = 25L;
+
+    // model data
+    final Builder builder = new Builder(dom_id);
+    builder.setTermFrequency(new BytesRef("foo"), 12L);
+    builder.setTermFrequency(new BytesRef("bar"), 10L);
+    builder.setTermFrequency(new BytesRef("baz"), 3L);
+
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Document id differs.", dom_id, dom.id);
+    Assert.assertEquals("Unique term count differs.",
+        dom_termCount, dom.termCount());
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        3L, dom.tf(new BytesRef("baz")));
+
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 12L / (double) dom_tf, dom.relTf(new BytesRef("foo")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 10L / (double) dom_tf, dom.relTf(new BytesRef("bar")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 3L / (double) dom_tf, dom.relTf(new BytesRef("baz")), 0d);
+
+    Assert.assertEquals("Total term frequency differs.", dom_tf, dom.tf());
+  }
+
+  @SuppressWarnings("ImplicitNumericConversion")
+  @Test
+  public void builder_testGetModelMap()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+    // number of unique terms
+    final int dom_termCount = 3;
+    // total term frequency
+    final long dom_tf = 25L;
+
+    final Builder builder = new Builder(dom_id);
+
+    // model data
+    final Map<BytesRef, Long> tfMap = new HashMap<>(6);
+    tfMap.put(new BytesRef("foo"), 12L);
+    tfMap.put(new BytesRef("bar"), 10L);
+    tfMap.put(new BytesRef("baz"), 3L);
+
+    builder.setTermFrequency(tfMap);
+    final DocumentModel dom = builder.getModel();
+
+    Assert.assertEquals("Document id differs.", dom_id, dom.id);
+    Assert.assertEquals("Unique term count differs.",
+        dom_termCount, dom.termCount());
+
+    Assert.assertEquals("Term frequency differs.",
+        12L, dom.tf(new BytesRef("foo")));
+    Assert.assertEquals("Term frequency differs.",
+        10L, dom.tf(new BytesRef("bar")));
+    Assert.assertEquals("Term frequency differs.",
+        3L, dom.tf(new BytesRef("baz")));
+
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 12L / (double) dom_tf, dom.relTf(new BytesRef("foo")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 10L / (double) dom_tf, dom.relTf(new BytesRef("bar")), 0d);
+    Assert.assertEquals("Relative term frequency value differs.",
+        (double) 3L / (double) dom_tf, dom.relTf(new BytesRef("baz")), 0d);
+
+    Assert.assertEquals("Total term frequency differs.", dom_tf, dom.tf());
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void builder_testGetModelMap_null()
+      throws Exception {
+    // document id
+    final int dom_id = 1;
+
+    final Builder builder = new Builder(dom_id);
+    try {
+      builder.setTermFrequency(null);
+      Assert.fail("No NullpointerException thrown.");
+    } catch (final NullPointerException e) {
+      // pass
+    }
   }
 }
