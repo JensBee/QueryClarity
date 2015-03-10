27edeead619b756e91f40617ba9cffa6d41ea5bf
Jens Bertram <code@jens-bertram.net>
2015-03-10 10:26:41 +0100

    add tests for DocumentModel class and serializers

---
+++ src/test/java/de/unihildesheim/iw/mapdb/serializer/DocumentModelSerializerTest.java
@@ -0,0 +1,20 @@
+package de.unihildesheim.iw.mapdb.serializer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DocumentModelSerializerTest {
+
+  @Test
+  public void testSerialize()
+      throws Exception {
+
+  }
+
+  @Test
+  public void testDeserialize()
+      throws Exception {
+
+  }
+}
